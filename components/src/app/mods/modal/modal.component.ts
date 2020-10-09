156541e421c1d4acf8af00bb350c15bd495dd08a
jadehendricks <hendricksjade@gmail.com>
2020-10-09 23:50:03 +0200

    opening, closing and making the modal reusable

--- components/src/app/mods/modal/modal.component.ts
+++ components/src/app/mods/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component, OnInit, ElementRef, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -7,10 +7,19 @@ import { Component, OnInit, ElementRef } from '@angular/core';
 })
 export class ModalComponent implements OnInit {
 
+  @Output() close = new EventEmitter();
+
   constructor(private el: ElementRef) {}
 
   ngOnInit(): void {
     document.body.appendChild(this.el.nativeElement);
   }
 
+  ngOnDestroy(): void {
+    this.el.nativeElement.remove();
+  }
+
+  onCloseClick(e: Event): void {
+    this.close.emit();
+  }
 }
--- components/src/app/mods/mods-home/mods-home.component.ts
+++ components/src/app/mods/mods-home/mods-home.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModsHomeComponent implements OnInit {
 
+  modalOpen = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onClick(): void {
+    this.modalOpen = !this.modalOpen;
   }
 
 }
