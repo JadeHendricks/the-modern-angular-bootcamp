5e949cf1bafb4240833dc3730d67c0ef1c6e08c1
jadehendricks <hendricksjade@gmail.com>
2020-10-11 21:16:41 +0200

    adding in a request to wikipedia and buidling the initial table

--- wsearch/src/app/search-bar/search-bar.component.ts
+++ wsearch/src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchBarComponent implements OnInit {
 
+  @Output() submitted = new EventEmitter<string>();
+  term = '';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onFormSubmit(event: any): void {
+    event.preventDefault();
+    this.submitted.emit(this.term);
+  }
 }
