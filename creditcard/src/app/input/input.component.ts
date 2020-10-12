e29c422df436456801c3c3ff332aef5edff55342
jadehendricks <hendricksjade@gmail.com>
2020-10-13 01:03:58 +0200

    working with reactive forms, formGroups formControls and error messages / styling

---
+++ creditcard/src/app/input/input.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, ValidationErrors } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css']
+})
+export class InputComponent implements OnInit {
+  @Input() control: FormControl;
+  @Input() label: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  showErrors(): ValidationErrors {
+    const { dirty, touched, errors } = this.control;
+    return dirty && touched && errors;
+  }
+
+}
