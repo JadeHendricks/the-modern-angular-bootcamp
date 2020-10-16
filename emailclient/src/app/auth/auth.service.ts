4e2874de5e03d0f7cf541de1bdb6d52d967c842f
jadehendricks <hendricksjade@gmail.com>
2020-10-16 15:45:26 +0200

    making a signup request, cleaning out the autService, adding generic error handling on the formGroup

--- emailclient/src/app/auth/auth.service.ts
+++ emailclient/src/app/auth/auth.service.ts
@@ -1,20 +1,37 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 interface UsernameAvailableResponse {
   available: boolean;
 }
+
+interface SignupCredentials {
+  username: string;
+  password: string;
+  passwordConfirmation: string;
+};
+
+interface SignupResponse {
+  username: string;
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  rootUrl: string = 'https://api.angular-email.com/auth';
+
   constructor(private http: HttpClient) { }
 
   usernameAvailable(username: string) {
-    return this.http.post<UsernameAvailableResponse>('https://api.angular-email.com/auth/username', {
+    return this.http.post<UsernameAvailableResponse>(`${this.rootUrl}/username`, {
       username
     });
+  }
+
+  signup(credentials: SignupCredentials): Observable<SignupResponse> {
+    return this.http.post<SignupResponse>(`${this.rootUrl}/signup`, credentials);
   }
 }
